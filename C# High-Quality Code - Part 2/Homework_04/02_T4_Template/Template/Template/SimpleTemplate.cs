227d9c8ac4e46795cee779732aa2e5c590b8eada
theshaftman <mariyan_apostolov@mail.bg>
2016-10-30 19:58:38 +0200

    High-Quality Code - Part 2 - Homework 04

---
+++ C# High-Quality Code - Part 2/Homework_04/02_T4_Template/Template/Template/SimpleTemplate.cs
@@ -0,0 +1,43 @@
+﻿namespace Template
+{
+	using System;
+	using System.Collections.Generic;
+
+	public class SimpleTemplate
+	{
+		// Returns the date from today.
+		public static DateTime Today()
+		{
+			DateTime date = DateTime.Parse("04 октомври 2016 г.");
+			return date;
+		}
+
+		// Prints the environment name.
+		public static void PrintEnvironmentName()
+		{
+			string name = "SAGER-PC";
+
+			List<string> fileNames = new List<string>()
+			{
+				name
+			};
+
+			foreach (string item in fileNames)
+			{
+				Console.WriteLine(item);
+			}
+		}
+
+		// Prints the current file names of a selected path.
+		public static void PrintCurrentFileNames(string currentPath)
+		{
+			if (System.IO.Directory.Exists(currentPath))
+			{
+				foreach (string item in System.IO.Directory.EnumerateFiles(currentPath))
+				{
+					Console.WriteLine(item.ToString());
+				}				
+			}
+		}
+	}
+}
